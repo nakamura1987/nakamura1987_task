cba3aa921be487c0a881844b8ee2567b723019c3
nakamura1987 <riserva.chianti@gmail.com>
2020-03-13 17:48:41 +0900

    アプリ修正２回目

--- db/migrate/20200310133256_create_tasks.rb
+++ db/migrate/20200310133256_create_tasks.rb
@@ -1,8 +1,6 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
-      t.string :title
-      t.text :body
       t.string :title, null: false
       t.string :body, null: false
       
